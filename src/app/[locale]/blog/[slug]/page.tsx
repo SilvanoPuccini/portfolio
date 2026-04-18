80a8e2a369c195f7472f60c345905d35a1e38161
Silvano Puccini <silvano.jm.puccini@gmail.com>
2026-04-18 00:11:01 -0300

    fix(blog): force scroll to top on post navigation to clear fixed header overlap

--- src/app/[locale]/blog/[slug]/page.tsx
+++ src/app/[locale]/blog/[slug]/page.tsx
@@ -7,6 +7,7 @@ import { PostCover } from "@/components/blog/PostCover";
 import { SubscribeForm } from "@/components/blog/SubscribeForm";
 import { resolveLocale, type Locale } from "@/lib/i18n";
 import { RadarBadge } from "@/components/blog/RadarBadge";
+import { ScrollToTop } from "@/components/site/ScrollToTop";
 
 type LocaleParams = Promise<{ locale: string; slug: string }>;
 
@@ -70,7 +71,8 @@ export default async function BlogPostPage({
   const restContent = hasIntro ? rawContent.slice(splitIndex + 1) : rawContent;
 
   return (
-    <div className="site-container pt-5 pb-10 sm:pt-6 sm:pb-12 lg:pt-8 lg:pb-16">
+    <div className="site-container py-10 sm:py-12 lg:py-16">
+      <ScrollToTop />
       <div className="mx-auto max-w-3xl">
 
         {/* Navegación superior */}
